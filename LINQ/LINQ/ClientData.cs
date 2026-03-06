9892268299929db72d988989471760d807450a7f
Opilane TTHK <opilane@tthk.ee>
2026-03-06 09:07:30 +0200

    Classid tehtud

---
+++ LINQ/LINQ/ClientData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class ClientData
+    {
+        public static readonly List<Client> clients = new List<Client>()
+        {
+             new Client() {},
+             new Client() {},
+             new Client() {},
+             new Client() {},
+             new Client() {},
+        };
+    }
+}
